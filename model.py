203a8371033b1fbdf07ed5b766744df80944ff95
Maura Thompson <thomaura@gmail.com>
2021-06-01 20:36:11 +0000

    Finish User class, create Landlord class in model.py

--- model.py
+++ model.py
@@ -17,3 +17,23 @@ class User(db.Model):
                         )
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
+
+    def __repr__(self):
+        return f"<User user_id={self.user_id} email={self.email}>"
+
+
+class Landlord(db.Model):
+    """A landlord"""
+
+    __tablename__ = "landlords"
+
+    landlord_id = db.Column(db.Integer,
+                            autoincrement=True,
+                            primary_key=True,
+                            )
+    landlord_name = db.Column(db.String)
+    office_address = db.Column(db.String)
+
+    def __repr__(self):
+        return f"<Landlord landlord_id={self.landlord_id} landlord_name={self.landlord_name}>"
+
