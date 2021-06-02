6fd73a152f7bc0ff792b4779f965000926d830de
Maura Thompson <thomaura@gmail.com>
2021-06-02 18:33:14 +0000

    Create README.md and seeD_database.py

---
+++ README.md
@@ -0,0 +1,3 @@
+Make sure you `createdb renters_speak`
+createdb in terminal, as bash command (not interactive)
+`db.create_all()` in python interactive, running model.py to creates tables in database
--- model.py
+++ model.py
@@ -90,3 +90,27 @@ class Review(db.Model):
 
     def __repr__(self):
         return f"<Review review_id={self.review_id} created_on={self.created_on}>"
+
+
+def connect_to_db(flask_app, db_uri='postgresql:///renters_speak', echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+
+
+if __name__ == '__main__':
+    # from server import app
+    from flask import Flask
+    app = Flask(__name__)
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
+
