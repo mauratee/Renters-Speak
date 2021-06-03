fbb58b88954cbffa5047549e4d05299f1048f692
Maura Thompson <thomaura@gmail.com>
2021-06-03 20:28:29 +0000

    Update server.py and README.md

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Make sure you `createdb testdb`
 `python3 seed_db.py`in terminal to seed database `testdb` with test data
 to view and query test database using SQL queries, enter `psql testdb` from terminal OR
 run `model.py` in interactive mode to query test database via SQLAlchemy queries
+
+TO RUN SERVER:
+in terminal, enter `python3 server.py`
+you should then see:
+Connected to the db!
+ * Serving Flask app 'server' (lazy loading)
+ then, in a web browser, navigate to `http://localhost:5000/`
+
--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 from flask import (Flask, render_template, request, flash, session,
                     redirect)
 from model import connect_to_db
-# import crud <-- un-comment afer crud.py complete
+import crud
 from jinja2 import StrictUndefined
 import os
 
@@ -11,8 +11,13 @@ app = Flask(__name__)
 app.secret_key = os.environ['secret_key']
 app.jinja_env.undefined = StrictUndefined
 
-# @app.route('/')
-# def render_homepage():
 
-#     return render_template('homepage.html')
-# <-- un comment after creating homepage.html
+@app.route('/')
+def render_homepage():
+
+    return render_template('homepage.html')
+
+
+if __name__ == '__main__':
+    connect_to_db(app)
+    app.run(host='0.0.0.0', debug=True)
