27cbf04e2732ff0098e90770268d3af3aa0be409
mauratee <mauraethompson@gmail.com>
2021-06-21 12:29:27 +0000

    Connect login.html to login.js, add alert to login-button

--- static/js/login.js
+++ static/js/login.js
@@ -1,17 +1,50 @@
 "use strict";
 
-// alert("JS is connected")
+
+alert("JS is connected");
+
+
+const loginForm = document.querySelector("form");
+
+loginForm.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+
+    alert("Success, you are logged in!")
+});
 
 // var myModal = document.getElementById('myModal')
 // var myInput = document.getElementById('myInput')
 
-var myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
+// var myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
 
-const button = document.querySelector("login_button");
+// const button = document.querySelector("login_button");
 
-        button.addEventListener('click', () => {
-          alert('Success, you are logged in!');
-        });
+//         button.addEventListener('click', () => {
+//           alert('Success, you are logged in!');
+//         });
+
+// $('#delivery-form').on('submit', (evt) => {
+//     evt.preventDefault();
+          
+//     // Get user input from a form
+//     const formData = {
+//         city: $('#city-field').val(),
+//         address: $('#adr-field').val()
+//         };
+
+// $("login-button").on("click", (evt) => {
+//     evt.preventDefault();
+
+//     alert("Success, you are logged in!")
+// });
+          
+//     // Send formData to the server (becomes a query string)
+//     $.get('/delivery-info.json', formData, (res) => {
+//         // Display response from the server
+//         alert(`This will cost $${res.cost}`);
+//         alert(`This will arrive in ${res.days} day(s)`);
+//         });
+// });
 
 
 // myModal.addEventListener('shown.bs.modal', function () {
