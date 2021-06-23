daca65ed27f9a19153fabaf0b61ece8c8656f592
mauratee <mauraethompson@gmail.com>
2021-06-23 02:08:13 +0000

    Add bootstrap formatting to building_details.html and work on slideup function in building_details.js

--- static/js/building_details.js
+++ static/js/building_details.js
@@ -3,11 +3,14 @@
 alert("JS is connected");
 
 // Selects from violation-group in building_details.html
-$("#violation-group").ready(function(){
-  $("show_fewer_violations").click(function(){
-    $("violation-list").slideUp();
+var violationList = {{ violation_list|safe }}
+console.log(violationList)
+
+$("document").ready(function(){
+  $("#show_fewer_violations").click(function(){
+    $("#first-two-violations").slideUp();
   });
-  $("show_more_violations").click(function(){
-    $("violation-list").slideDown();
+  $("#show_more_violations").click(function(){
+    $("#first-two-violations").slideDown();
   });
 });
