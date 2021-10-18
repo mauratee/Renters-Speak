9a54f3a7615fd2be1f99a30d3af074c0ea791dcc
Maura Thompson <thomaura@gmail.com>
2021-10-18 10:37:38 -0400

    update search_address.js and add comments

--- static/js/search_address.js
+++ static/js/search_address.js
@@ -3,24 +3,7 @@
 // alert("JS is connected");
 
 
-// // Add event listener input type to detect any change to input field
-// $("#search_nyc_address").on("input", () => {
-
-//     // Get user input from search form, save in dictionary
-//     const formData = {
-//         text: $("#search_nyc_address").val(),
-//     };
-
-//     // Send formData to the server
-//     $.get("/autocomplete", formData, (res) => {
-//         for item in res () =>
-//             var optionNode = document.createElement("option");
-//             optionNode.value = "//address1//";
-//             optionNode.appendChild(document.createTextNode("//firstaddresslabel//"));
-//             document.getElementById("addresses").appendChild(optionNode);
-//     })
-// })
-
+// make ajax request to "/autocomplete" route as user types address in search bar
 $(function() {
     $("#search_nyc_address").autocomplete({
         source:function(request, response) {
@@ -33,7 +16,7 @@ $(function() {
         },
         minLength: 1,
         select: function(event, ui) {
-            console.log(ui.item.value); // not in your question, but might help later
+            console.log(ui.item.value);
         }
     });
 })
